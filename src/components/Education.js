5ab5b33d9135733d807ef33636fb54dd493c0033
Shivani-hub-cell <shivanipanchal369@gmail.com>
2025-08-30 12:29:57 +0530

    Update portfolio: add resume, projects, design improvements

--- src/components/Education.js
+++ src/components/Education.js
@@ -8,11 +8,11 @@ const Education = () => {
       <ul className="timeline">
         <li>
           <h3>Bachelor of Computer Science</h3>
-          <p>Rajasthan University (St.Wilfred Collage)(2020 - 2024)</p>
+          <p>Rajasthan University (St.Wilfred Collage)</p>
         </li>
         <li>
           <h3>12th Grade</h3>
-          <p>Vikash International School (2019 - 2020)</p>
+          <p>Vikash International School</p>
         </li>
       </ul>
     </section>
